8f09e4450bf98cdd499fe6245f66abe90b6da672
Margot Rasamy <rasamymargot@gmail.com>
2023-11-08 18:33:29 +0100

    landing page hero

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,16 @@
 import './styles/main.scss';
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
-import Header from './components/Header';
 import LandingPage from './components/pages/landing-page/LandingPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header />
-        <div className="app-content">
-          <Routes>
-              <Route exact path="/" element={<LandingPage/>} />
-          </Routes>
-        </div>
+
+        <Routes>
+            <Route exact path="/" element={<LandingPage/>} />
+        </Routes>
+        
       </div>
     </BrowserRouter>
   );
--- frontend/src/components/pages/landing-page/LandingPage.jsx
+++ frontend/src/components/pages/landing-page/LandingPage.jsx
@@ -1,4 +1,25 @@
+// import banner from '../../../assets/images/photos/banner3.jpg';
+import Header from '../../Header';
+
 const LandingPage = () => {
-    
+    return (
+        <div className="landing-page">
+            <div className='hero'>
+                <Header />
+                <div className='background'>
+                    <div className='description'>
+                        <h1 className='title'>Hazavana Hotel</h1>
+                    </div>
+                    <div className='overlay'></div>
+                </div>
+                {/* <div className='image-holder'>
+                    <img src={banner} alt="banner" />
+                </div> */}
+            </div>
+            <div className='app-content'>
+               
+            </div>
+        </div>
+    );
 }
 export default LandingPage;
