b8bea97f8aa59c430a3fb539f02921a0cdaf4169
Margot Rasamy <rasamymargot@gmail.com>
2023-11-14 19:08:00 +0100

    Update logo and hero page

--- frontend/src/components/pages/landing-page/LandingPage.jsx
+++ frontend/src/components/pages/landing-page/LandingPage.jsx
@@ -1,4 +1,5 @@
 // import banner from '../../../assets/images/photos/banner3.jpg';
+import scrollArrow from '../../../assets/images/utils/scroll_arrow.png';
 import Header from '../../Header';
 
 const LandingPage = () => {
@@ -6,15 +7,16 @@ const LandingPage = () => {
         <div className="landing-page">
             <div className='hero'>
                 <Header />
-                <div className='background'>
+                <div className='hero-content'>
                     <div className='description'>
-                        <h1 className='title'>Hazavana Hotel</h1>
+                        <h1 className='title'>Hazavana Lodge</h1>
+                        <p>Hazavana : une lumière, une couleur dans sa diversité, un rayonnement avec l'originalité de chacun.</p>
+                    </div>
+                    <div className='bottom-hero'>
+                        <a href="#" className="scroll-arrow"><img src={scrollArrow} alt="scroll"/></a>
                     </div>
                     <div className='overlay'></div>
                 </div>
-                {/* <div className='image-holder'>
-                    <img src={banner} alt="banner" />
-                </div> */}
             </div>
             <div className='app-content'>
                
